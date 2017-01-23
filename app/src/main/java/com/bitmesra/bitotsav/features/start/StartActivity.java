9020a98b05585b72960929afa436084bd90f9dbc
GauravChaddha1996 <chaddhag@gmail.com>
2017-01-24 00:20:36 +0530

    Add animation to bitotsav logo in start activity

--- app/src/main/java/com/bitmesra/bitotsav/features/start/StartActivity.java
+++ app/src/main/java/com/bitmesra/bitotsav/features/start/StartActivity.java
@@ -1,17 +1,24 @@
 package com.bitmesra.bitotsav.features.start;
 
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.widget.ImageView;
 
 import com.bitmesra.bitotsav.R;
 import com.bitmesra.bitotsav.features.base.BaseAppCompatActivity;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class StartActivity extends BaseAppCompatActivity implements StartViewInterface {
 
-    StartPresenterImpl StartPresenter;
+    @BindView(R.id.startLogo)
+    ImageView startLogo;
+    private StartPresenterImpl StartPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,11 @@ public class StartActivity extends BaseAppCompatActivity implements StartViewInt
         setContentView(R.layout.activity_start);
         ButterKnife.bind(this);
         StartPresenter = new StartPresenterImpl(this);
+        ObjectAnimator animator = ObjectAnimator.ofFloat(startLogo,"rotationY",0,360).setDuration(2500);
+        animator.setInterpolator(new AnticipateOvershootInterpolator());
+        animator.setRepeatMode(ValueAnimator.REVERSE);
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.start();
     }
 
 
