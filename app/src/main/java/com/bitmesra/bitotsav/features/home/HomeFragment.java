9b7447f590f3cf8987146363bcbf92a92b53af85
GauravChaddha1996 <chaddhag@gmail.com>
2017-01-26 15:31:40 +0530

    Add home flagship event slider

--- app/src/main/java/com/bitmesra/bitotsav/features/home/HomeFragment.java
+++ app/src/main/java/com/bitmesra/bitotsav/features/home/HomeFragment.java
@@ -9,18 +9,40 @@ import android.view.ViewGroup;
 import com.bitmesra.bitotsav.R;
 import com.bitmesra.bitotsav.base.BaseFragment;
 import com.bitmesra.bitotsav.features.IdForFragment;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 
 public class HomeFragment extends BaseFragment {
 
+    @BindView(R.id.homeSliderLayout)
+    SliderLayout homeSliderLayout;
 
     public HomeFragment() {
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        ButterKnife.bind(this, view);
+        setUpSliderLayout();
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        homeSliderLayout.startAutoCycle(4000, 4000, true);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        homeSliderLayout.stopAutoCycle();
     }
 
     @Override
@@ -31,5 +53,27 @@ public class HomeFragment extends BaseFragment {
     @Override
     public IdForFragment getBackToClazz() {
         return null;
+    }
+
+    private void setUpSliderLayout() {
+        TextSliderView view = new TextSliderView(getActivity());
+        view.description("Bitotsav 17")
+                .image(R.drawable.home1);
+        TextSliderView view2 = new TextSliderView(getActivity());
+        view2.description("Party at BIT 17")
+                .image(R.drawable.home2);
+        TextSliderView view3 = new TextSliderView(getActivity());
+        view3.description("Party3 at BIT 17")
+                .image(R.drawable.home1);
+        TextSliderView view4 = new TextSliderView(getActivity());
+        view4.description("Party4 at BIT 17")
+                .image(R.drawable.home2);
+        homeSliderLayout.addSlider(view);
+        homeSliderLayout.addSlider(view2);
+        homeSliderLayout.addSlider(view3);
+        homeSliderLayout.addSlider(view4);
+        homeSliderLayout.setPresetTransformer(SliderLayout.Transformer.ZoomOut);
+        homeSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        homeSliderLayout.setDuration(4000);
     }
 }
