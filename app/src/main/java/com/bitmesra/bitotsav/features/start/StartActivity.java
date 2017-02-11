a42df2da00d045d43071c845c13d8f4681841689
GauravChaddha1996 <chaddhag@gmail.com>
2017-02-11 15:24:55 +0530

    Fix start screen design

--- app/src/main/java/com/bitmesra/bitotsav/features/start/StartActivity.java
+++ app/src/main/java/com/bitmesra/bitotsav/features/start/StartActivity.java
@@ -1,10 +1,8 @@
 package com.bitmesra.bitotsav.features.start;
 
+import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.graphics.drawable.AnimatedVectorDrawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
@@ -13,8 +11,6 @@ import android.widget.TextView;
 
 import com.bitmesra.bitotsav.R;
 import com.bitmesra.bitotsav.features.MainActivity;
-import com.daimajia.androidanimations.library.Techniques;
-import com.daimajia.androidanimations.library.YoYo;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,14 +19,10 @@ public class StartActivity extends AppCompatActivity implements StartViewInterfa
 
     @BindView(R.id.startLogo)
     ImageView startLogo;
-    @BindView(R.id.startBitotsavTitle)
-    TextView startBitotsavTitle;
-    @BindView(R.id.startSponsor)
-    TextView startSponsor;
+    @BindView(R.id.startBlurLogo)
+    ImageView startBlurLogo;
     @BindView(R.id.startPPRText)
     TextView startPPRText;
-    @BindView(R.id.startPPRImage)
-    ImageView startPPRImage;
 
     private StartPresenterImpl startPresenter;
 
@@ -59,32 +51,38 @@ public class StartActivity extends AppCompatActivity implements StartViewInterfa
 
     //Handles all animation on this start activity
     private void startAnimation() {
-        YoYo.with(Techniques.FadeInDown)
-                .duration(2000)
-                .playOn(startLogo);
-        YoYo.with(Techniques.SlideInRight)
-                .duration(2000)
-                .playOn(startBitotsavTitle);
-        YoYo.with(Techniques.FlipInX)
-                .duration(2000)
-                .playOn(startSponsor);
-        YoYo.with(Techniques.Landing)
-                .duration(2000)
-                .playOn(startSponsor);
-        YoYo.with(Techniques.SlideInUp)
-                .duration(2000)
-                .playOn(startPPRText);
-        AnimatedVectorDrawable animatedVectorDrawable = (AnimatedVectorDrawable) getBaseContext().
-                getDrawable(R.drawable.play_pause_repeat_animated_vector);
-        startPPRImage.setImageDrawable(animatedVectorDrawable);
-        Handler handler = new Handler() {
-            @Override
-            public void handleMessage(Message msg) {
-                animatedVectorDrawable.start();
-                sendEmptyMessageDelayed(0, 500);
-            }
-        };
-        handler.sendEmptyMessage(0);
+        ObjectAnimator scaleDownX = ObjectAnimator.ofFloat(startLogo, "scaleX", 0.8f, 1f);
+        ObjectAnimator scaleDownY = ObjectAnimator.ofFloat(startLogo, "scaleY", 0.8f, 1f);
+        scaleDownX.setDuration(2000);
+        scaleDownY.setDuration(2000);
+        scaleDownY.start();
+        scaleDownX.start();
+        ObjectAnimator alpha = ObjectAnimator.ofFloat(startLogo, "alpha", 0f, 1f);
+        alpha.setDuration(1000);
+        alpha.start();
+
+        ObjectAnimator translateY = ObjectAnimator.ofFloat(startBlurLogo, "translationY", 0f, 300f);
+        translateY.setDuration(2000);
+        translateY.start();
+        ObjectAnimator alphaBlur = ObjectAnimator.ofFloat(startBlurLogo, "alpha", 0f, 1f);
+        alphaBlur.setDuration(1000);
+        alphaBlur.start();
+        ObjectAnimator alphaBlur2 = ObjectAnimator.ofFloat(startBlurLogo, "alpha", 1f, 0.2f);
+        alphaBlur2.setDuration(1000).setStartDelay(1000);
+        alphaBlur2.start();
+
+        ObjectAnimator finalscaleDownX = ObjectAnimator.ofFloat(startLogo, "scaleX", 1f, 25f);
+        ObjectAnimator finalscaleDownY = ObjectAnimator.ofFloat(startLogo, "scaleY", 1f, 25f);
+        finalscaleDownX.setStartDelay(2500);
+        finalscaleDownY.setStartDelay(2500);
+        finalscaleDownY.setDuration(500);
+        finalscaleDownX.setDuration(500);
+        finalscaleDownY.start();
+        finalscaleDownX.start();
+        ObjectAnimator alphaBlurOut = ObjectAnimator.ofFloat(startBlurLogo, "alpha", 0.2f, 0f);
+        alphaBlurOut.setStartDelay(2500);
+        alphaBlurOut.setDuration(500);
+        alphaBlurOut.start();
     }
 
 
